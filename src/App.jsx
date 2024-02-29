e8672aa8b7d2b4b0ea5b4626054c029fa65d31a0
cowd90 <tienn.dungg.980@gmail.com>
2024-02-29 11:06:29 +0700

    fp

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import React from 'react'
+import './App.css'
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css'
+import '../node_modules/bootstrap/dist/js/bootstrap.min.js'
+import AddRoom from './components/room/AddRoom'
+import NavBar from './components/layout/NavBar.jsx'
+import Footer from './components/layout/Footer.jsx'
+import ExistingRooms from './components/room/ExistingRooms.jsx'
+import Home from "./components/home/Home.jsx";
+import EditRoom from "./components/room/EditRoom.jsx";
+import BookingSuccess from "./components/bookings/BookingSuccess.jsx";
+import Bookings from "./components/bookings/Bookings.jsx"
+import Checkout from "./components/bookings/Checkout.jsx";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import RoomListing from './components/room/RoomListing.jsx'
+import Admin from './components/admin/Admin.jsx'
+import FindBooking from './components/bookings/FindBooking.jsx'
+import Login from './components/auth/Login.jsx'
+import Registration from './components/auth/Registration.jsx'
+import Profile from './components/auth/Profile.jsx'
+import Logout from './components/auth/Logout.jsx'
+import { AuthProvider } from './components/auth/AuthProvider.jsx'
+
+function App() {
+  return (
+    <AuthProvider>
+    <main>
+      <Router>
+        <NavBar/>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/edit-room/:roomId' element={<EditRoom/>}/>
+          <Route path='/existing-rooms' element={<ExistingRooms/>}/>
+          <Route path='/add-room' element={<AddRoom/>}/>
+          <Route path='/book-room/:roomId' element={<Checkout/>}/>
+          <Route path='/browse-all-rooms' element={<RoomListing/>}/>
+          <Route path='admin' element={<Admin/>}/>
+          <Route path="/booking-success" element={<BookingSuccess/>}/>
+          <Route path="/existing-bookings" element={<Bookings/>}/>
+          <Route path="/find-booking" element={<FindBooking/>}/>
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Registration/>}/>
+          <Route path="/profile" element={<Profile/>}/>
+          <Route path="/logout" element={<Logout/>}/>
+        </Routes>
+      </Router>
+      <Footer/>
+    </main>
+    </AuthProvider> 
+  )
+}
+
+export default App
